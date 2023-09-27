1fa0c7606e061e4d910e75c25890b42e9daeacd1
Billy Lynch <wlynch92@gmail.com>
2023-09-27 13:53:24 -0400

    add container start
    + other things I'm probably forgetting.

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 )
 
 require (
+	github.com/Azure/go-ansiterm v0.0.0-20210617225240-d185dfc1b5a1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/docker/distribution v2.8.2+incompatible // indirect
@@ -35,8 +36,10 @@ require (
 	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/moby/pubsub v1.0.0 // indirect
+	github.com/moby/term v0.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.0.2 // indirect
--- server/main.go
+++ server/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"bufio"
+	"context"
 	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -10,10 +13,13 @@ import (
 	"strings"
 	"time"
 
+	"github.com/docker/docker/api/server/httputils"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/events"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/network"
+	"github.com/docker/docker/pkg/stdcopy"
 	"github.com/gorilla/mux"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -29,7 +35,24 @@ type CreateRequest struct {
 	NetworkingConfig *network.NetworkingConfig
 }
 
+var (
+	clientset *kubernetes.Clientset
+)
+
 func main() {
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(clientcmd.NewDefaultClientConfigLoadingRules(), nil).ClientConfig()
+	if err != nil {
+		panic(err)
+	}
+
+	// create the clientset
+	clientset, err = kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err)
+	}
+
 	r := mux.NewRouter()
 	r.HandleFunc("/_ping", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.URL.Path)
@@ -71,8 +94,137 @@ func main() {
 		}
 
 	})
-	r.HandleFunc("/v1.24/containers/{id}/json", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/v1.24/images/{owner}/{image}/json", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.URL.Path)
+	})
+	r.HandleFunc("/v1.24/containers/{container}/json", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+		json.NewEncoder(w).Encode(types.ContainerJSON{
+			ContainerJSONBase: &types.ContainerJSONBase{
+				State: &types.ContainerState{
+					Running: true,
+				},
+			},
+		})
+	})
+	r.HandleFunc("/v1.24/containers/buildx_buildkit_default/exec", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+		raw, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		fmt.Println(string(raw))
+
+		data := new(CreateRequest)
+		if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		defer r.Body.Close()
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		if err := enc.Encode(data); err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		data.Image = "moby/buildkit"
+
+		ctx := r.Context()
+		resp, err := createContainer(ctx, data)
+		if err != nil {
+			switch {
+			case errors.IsAlreadyExists(err):
+				http.Error(w, fmt.Sprint(err), http.StatusConflict)
+			default:
+				http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+				return
+			}
+		}
+		json.NewEncoder(os.Stdout).Encode(resp)
+		json.NewEncoder(w).Encode(resp)
+	})
+	r.HandleFunc("/v1.24/exec/{container}/start", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.URL.Path)
+
+		raw, err := httputil.DumpRequest(r, true)
+		if err != nil {
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		fmt.Println(string(raw))
+
+		io.ReadAll(r.Body)
+
+		container := mux.Vars(r)["container"]
+
+		if err := waitForReady(r.Context(), "forever", container); err != nil {
+			fmt.Println(err)
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+
+		req := clientset.CoreV1().Pods("default").GetLogs("forever", &corev1.PodLogOptions{
+			Container: container,
+		})
+		podLogs, err := req.Stream(r.Context())
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		defer podLogs.Close()
+
+		inStream, outStream, err := httputils.HijackConnection(w)
+		if err != nil {
+			fmt.Println(err)
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		defer httputils.CloseStreams(inStream, outStream)
+		if _, ok := r.Header["Upgrade"]; ok {
+			contentType := types.MediaTypeRawStream
+			fmt.Fprint(outStream, "HTTP/1.1 101 UPGRADED\r\nContent-Type: "+contentType+"\r\nConnection: Upgrade\r\nUpgrade: tcp\r\n")
+		} else {
+			fmt.Fprint(outStream, "HTTP/1.1 200 OK\r\nContent-Type: application/vnd.docker.raw-stream\r\n")
+		}
+		// copy headers that were removed as part of hijack
+		if err := w.Header().WriteSubset(outStream, nil); err != nil {
+			fmt.Println(err)
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
+		fmt.Fprint(outStream, "\r\n")
+
+		stdin := inStream
+		stdout := outStream
+
+		//stderr := stdcopy.NewStdWriter(outStream, stdcopy.Stderr)
+		stdout = stdcopy.NewStdWriter(outStream, stdcopy.Stderr)
+
+		/*
+			w.Header().Set("Content-Type", "application/vnd.docker.raw-stream")
+			w.WriteHeader(200)
+		*/
+
+		go func() {
+			reader := bufio.NewReader(stdin)
+			for {
+				line, err := reader.ReadBytes('\n')
+				if err != nil {
+					return
+				}
+				fmt.Println(string(line))
+			}
+		}()
+
+		//sw := stdcopy.NewStdWriter(stdout, stdcopy.Stderr)
+
+		//sw2 := stdcopy.NewStdWriter(w, stdcopy.Stdout)
+
+		io.Copy(stdout, io.TeeReader(podLogs, os.Stdout))
+
+		//<-r.Cancel
 	})
 	r.HandleFunc("/v1.24/containers/{id}/start", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.URL.Path)
@@ -91,6 +243,26 @@ func main() {
 	})
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		panic(err)
+	}
+}
+
+func waitForReady(ctx context.Context, pod, container string) error {
+	for {
+		if ctx.Err() != nil {
+			return ctx.Err()
+		}
+		pod, err := clientset.CoreV1().Pods("default").Get(ctx, pod, metav1.GetOptions{})
+		if err != nil {
+			return err
+		}
+		for _, c := range pod.Status.EphemeralContainerStatuses {
+			if c.Name == container {
+				if c.State.Running != nil {
+					return nil
+				}
+			}
+		}
+		time.Sleep(1 * time.Second)
 	}
 }
 
@@ -117,23 +289,25 @@ func containerCreate(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	// use the current context in kubeconfig
-	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(clientcmd.NewDefaultClientConfigLoadingRules(), nil).ClientConfig()
+	resp, err := createContainer(ctx, data)
 	if err != nil {
-		panic(err.Error())
+		switch {
+		case errors.IsAlreadyExists(err):
+			http.Error(w, fmt.Sprint(err), http.StatusConflict)
+		default:
+			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
+			return
+		}
 	}
+	json.NewEncoder(os.Stdout).Encode(resp)
+	json.NewEncoder(w).Encode(resp)
+}
 
-	// create the clientset
-	clientset, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		panic(err.Error())
-	}
+func createContainer(ctx context.Context, data *CreateRequest) (*container.CreateResponse, error) {
 
 	pod, err := clientset.CoreV1().Pods("default").Get(ctx, "forever", metav1.GetOptions{})
 	if err != nil {
-		fmt.Println(err)
-		http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-		return
+		return nil, err
 	}
 
 	ec := corev1.EphemeralContainer{
@@ -148,22 +322,12 @@ func containerCreate(w http.ResponseWriter, req *http.Request) {
 
 	_, err = clientset.CoreV1().Pods("default").UpdateEphemeralContainers(ctx, "forever", pod, metav1.UpdateOptions{})
 	if err != nil {
-		fmt.Println(err)
-
-		switch {
-		case errors.IsAlreadyExists(err):
-			http.Error(w, fmt.Sprint(err), http.StatusConflict)
-		default:
-			http.Error(w, fmt.Sprint(err), http.StatusInternalServerError)
-			return
-		}
+		return nil, err
 	}
 
-	resp := container.CreateResponse{
+	return &container.CreateResponse{
 		ID: ec.Name,
-	}
-	json.NewEncoder(os.Stdout).Encode(resp)
-	json.NewEncoder(w).Encode(resp)
+	}, nil
 }
 
 var (
